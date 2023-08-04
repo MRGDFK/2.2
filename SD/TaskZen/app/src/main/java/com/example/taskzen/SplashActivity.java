317227dc04420879c681b28cdf009f28a6ad363d
MRGDFK <sharjilskhan3771@gmail.com>
2023-08-04 21:23:32 +0600

    Google Sign IN ADDED!

--- SD/TaskZen/app/src/main/java/com/example/taskzen/SplashActivity.java
+++ SD/TaskZen/app/src/main/java/com/example/taskzen/SplashActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.taskzen.logIn.logIn_activity;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -17,10 +19,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent iHome = new Intent(SplashActivity.this, MainActivity.class);
+                Intent iHome = new Intent(SplashActivity.this, logIn_activity.class);
                 startActivity(iHome);
             }
-        }, 4000);
+        }, 2000);
 
 
 
